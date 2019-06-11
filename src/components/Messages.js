f1b979914b879c0bd07d43d2442f63fa60981aaf
Mina Shin <minamishin@hotmail.com>
2019-06-11 15:51:07 -0700

    input functions for messages

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,5 +1,26 @@
 import React from 'react';
-import 
+import Timestamp from './Timestamp';
 
+const chatTimestamp = (props) => {
+    return (
+      <Timestamp time={ props } />
+    );
+  };
+  
+  const Messages = (props) => {
+    return (
+      <section>
+        <h3>
+          { props.sender }
+        </h3>
+        <p>
+          { props.body }
+        </p>
+        <p>
+          { chatTimestamp(props.timeStamp) }
+        </p>
+      </section>
+    );
+  };
 
 export default Messages;
