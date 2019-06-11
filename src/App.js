2ecc47f3f0e2a3dd5b5d118a9290e1fad2352810
Kate Sandler <kate@katekatekate.net>
2019-06-11 16:02:06 -0700

    Update App.js to reflect chatlog/entries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import Log from './components/Log';
 
 const App = () => {
   console.log(chatMessages);
@@ -8,10 +9,10 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">A chat.</h1>
       </header>
       <main className="App-main">
-
+        <Log messages={chatMessages} />
       </main>
     </div>
   );
