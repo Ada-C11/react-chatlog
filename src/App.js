8a9df0bc6cc314ff1804aa9009ac044118f38e7f
qqdipps <sdipp007@gmail.com>
2019-06-11 15:37:00 -0700

    added names dynamic for chat application

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import chatMessages from './data/messages.json';
 import ChatContainer from './components/ChatContainer';
 
 const App = () => {
+  const names = [];
+  chatMessages.forEach(object => {
+    if (!names.includes(object.sender)) {
+      names.push(object.sender);
+    }
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">
+          Chat Between {names[0]} and {names[1]}
+        </h1>
       </header>
       <main className="App-main" />
       <ChatContainer messages={chatMessages} />
