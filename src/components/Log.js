1a11b399435db982f6f34134d42b5e28f35403cb
Kate Sandler <kate@katekatekate.net>
2019-06-11 15:50:42 -0700

    Creates a chatlog component to hold messages

--- src/components/Log.js
+++ src/components/Log.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import Message from './Message'
-import { tsPropertySignature } from '@babel/types';
+import Message from './Message';
 
-const Log = (props) => {
+const Log = props => {
   const messages = props.entries.map((entry, i) => {
     const sender = i % 2 == 0 ? 'local' : 'remote';
-    return <Message message =
-  })
-  return (
-
-  );
-}
+    return <Message message={{ ...entry, sender }} />;
+  });
+  return <section className="chat-log">{messages}</section>;
+};
 
 export default Log;
