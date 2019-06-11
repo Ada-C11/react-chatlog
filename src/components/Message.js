b8804aa700bef4540bcef70bba6174a5e76aff33
Kate Sandler <kate@katekatekate.net>
2019-06-11 15:41:00 -0700

    Builds a message component

---
+++ src/components/Message.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Timestamp from './Timestamp';
+
+const Message = props => {
+  return (
+    <article>
+      <h1 className="entry-name">{props.message.sender}</h1>
+      <div className="entry-bubble">
+        <p className="entry-body">{props.message.body}</p>
+        <p className="entry-time">
+          <Timestamp time={props.message.timeStamp} />
+        </p>
+      </div>
+    </article>
+  );
+};
+
+export default Message;
