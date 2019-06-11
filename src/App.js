db3f845057ac59c0f396e045f9b9a3eef7d51ba2
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-06-11 16:41:57 -0700

    added static message

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 
+import Message from './components/Message';
+
 const App = () => {
   console.log(chatMessages);
 
@@ -11,7 +13,7 @@ const App = () => {
         <h1 className="App-title">Application title</h1>
       </header>
       <main className="App-main">
-
+        <Message />
       </main>
     </div>
   );
---
+++ src/components/Message.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Timestamp from './Timestamp';
+
+const Message = () => {
+    const staticMessages = [
+        {
+          "sender":"Vladimir",
+          "body":"why are you arguing with me",
+          "timeStamp":"2018-05-29T22:49:06+00:00"
+        },
+        {
+          "sender":"Estragon",
+          "body":"Because you are wrong.",
+          "timeStamp":"2018-05-29T22:49:33+00:00"
+        },
+    ];
+
+    return (
+      <section>
+        <p> {staticMessages[0]["sender"]} </p>
+        <p> {staticMessages[0]["body"]} </p>
+        <p> <Timestamp time={staticMessages[0]["timeStamp"]} /> </p>
+      </section>
+    );
+  }
+  
+  export default Message;
