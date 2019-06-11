166803bd5b36e69ce58cb6d2441a2a5a9836f33e
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-06-11 16:58:49 -0700

    accessed props from App.js to get all chat messages

--- src/components/MessageLog.js
+++ src/components/MessageLog.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Message from './Message';
 
-const MessageLog = () => {
+const MessageLog = (props) => {
+
+    const accessJsonMessages = props.allMessages.map( (message, i) => {
+        return (
+            <div key={i}>
+                <Message 
+                    sender={message.sender}
+                    body={message.body}
+                    timeStamp={message.timeStamp}
+                />
+            </div>
+        ); 
+    });
+
     return (
-        <div>
-            <Message 
-                sender={}
-                body={}
-                timeStamp={}
-            />
-        </div>
+        { accessJsonMessages }
     );
 };
 
