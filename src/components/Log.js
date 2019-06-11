4b57b80f7af367437d7788f79a4a28b9c11e9280
Kate Sandler <kate@katekatekate.net>
2019-06-11 16:42:09 -0700

    Messages styled according to sender

--- src/components/Log.js
+++ src/components/Log.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Message from './Message';
 
 const Log = props => {
-  const messages = props.entries.map((entry, i) => {
-    const sender = i % 2 == 0 ? 'local' : 'remote';
+  const messages = props.messages.map((entry, i) => {
+    const sender = i % 2 === 0 ? 'local' : 'remote';
     return <Message message={{ ...entry, sender }} />;
   });
   return <section className="chat-log">{messages}</section>;
