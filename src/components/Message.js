75b78511a7fd5a4c8a4fb34527a8be2d24c2b3d3
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-06-11 17:55:13 -0700

    cleaned up ternary

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Timestamp from './Timestamp';
 
 const Message = (props) => {
+    let sentFrom = props.sender == 'Vladimir' ? 'local' : 'remote';
+
     return (
-      <section className={`chat-log chat-entry ${props.sender == 'Vladimir' ? 'local' : 'remote'}`}>
+      <section className={`chat-log chat-entry ${sentFrom}`}>
         <p className="entry-name"> {props.sender} </p>
         <section className="entry-bubble">
             <p className="entry-body"> {props.body} </p>
