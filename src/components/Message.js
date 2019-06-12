9d7dabea2c71853f8dc945852d84b56593b28915
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-06-11 17:50:53 -0700

    added conditional styling so message would be different depending on sender

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Timestamp from './Timestamp';
 
 const Message = (props) => {
+    const sentFrom = props.sender == "Vladimir";
+
     return (
-      <section className="chat-entry local">
-        <p> {props.sender} </p>
-        <p> {props.body} </p>
-        <p> <Timestamp time={props.timeStamp} /> </p>
+      <section className={`chat-entry ${sentFrom ? 'local' : 'remote'}`}>
+        <p className="entry-name"> {props.sender} </p>
+        <section className="entry-bubble">
+            <p className="entry-body"> {props.body} </p>
+            <p className="entry-time"> <Timestamp time={props.timeStamp} /> </p>
+        </section>
       </section>
     );
   }
