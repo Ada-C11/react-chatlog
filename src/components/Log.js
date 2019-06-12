98b8f684d8361d221d403329b28c4e11e39c7377
Kate Sandler <kate@katekatekate.net>
2019-06-11 17:17:30 -0700

    Refactored conditional formatting in case senders don't take turns

--- src/components/Log.js
+++ src/components/Log.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Message from './Message';
 
 const Log = props => {
+  const local = props.messages[0].sender;
   const messages = props.messages.map((entry, i) => {
-    const person = i % 2 === 0 ? 'local' : 'remote';
+    const person = entry.sender === local ? 'local' : 'remote';
     return <Message message={{ ...entry, person }} />;
   });
   return <section className="chat-log">{messages}</section>;
